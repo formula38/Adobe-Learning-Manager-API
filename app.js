294a616685ede024966ac31ad88c5ea130a1ae56
formula38 <louismconley@gmail.com>
2023-07-25 11:09:50 -0700

    move index.ejs to src folder

---
+++ app.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let path = require('path');
+let app = express();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
+
+
+

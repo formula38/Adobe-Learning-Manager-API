06ac4dd7778badf022a4d8cc8d68982c51933ab0
formula38 <louismconley@gmail.com>
2023-07-26 16:41:38 -0700

    HTML, CSS, JS updates

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function getUser() {
-    const userCountValue = 100;
-    const userCountElement = document.getElementById("userCount");
+function getUsers() {
+    fetch('https://captivateprime.adobe.com/primeapi/v2/users?page[offset]=0&page[limit]=10&sort=id')
+        .then(response => response.json())
+        .then(data => {
+            const userListElement = document.getElementById('userList');
+            userListElement.innerHTML = ''; // Clear existing user list
 
-    userCountElement.textContent = `Number of Users: ${userCountValue}`;
+            data.data.forEach(user => {
+                const name = user.attributes.name;
+                const email = user.attributes.email;
+                const profile = user.attributes.profile;
+
+                const userDiv = document.createElement('div');
+                userDiv.innerHTML = `
+                  <h3>Name: ${name}</h3>
+                  <p>Email: ${email}</p>
+                  <p>Profile: ${profile}</p>
+                  <hr>
+                `;
+
+                userListElement.appendChild(userDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }

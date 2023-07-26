94642fee619b27b194d5e7c9a04bc673e675af41
formula38 <louismconley@gmail.com>
2023-07-25 17:47:57 -0700

    HTML, CSS, JS updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getUser() {
     const userCountValue = 100;
     const userCountElement = document.getElementById("userCount");
+
     userCountElement.textContent = `Number of Users: ${userCountValue}`;
-    console.log('All Active ABC Corp ALM Users...');
 }

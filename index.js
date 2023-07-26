8ea9acf11519bee02529c481d9281565aa1bacb5
formula38 <louismconley@gmail.com>
2023-07-26 16:57:58 -0700

    HTML, CSS, JS updates

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-function getUsers() {
-    fetch('https://captivateprime.adobe.com/primeapi/v2/users?page[offset]=0&page[limit]=10&sort=id')
-        .then(response => response.json())
-        .then(data => {
-            const userListElement = document.getElementById('userList');
-            userListElement.innerHTML = ''; // Clear existing user list
+async function getUsers() {
+    try {
+        const response = await fetch('https://captivateprime.adobe.com/primeapi/v2/users?page[offset]=0&page[limit]=10&sort=id');
 
-            data.data.forEach(user => {
-                const name = user.attributes.name;
-                const email = user.attributes.email;
-                const profile = user.attributes.profile;
+        const responseData = await response.text();
+        console.log('API Response:', responseData);
 
-                const userDiv = document.createElement('div');
-                userDiv.innerHTML = `
-                  <h3>Name: ${name}</h3>
-                  <p>Email: ${email}</p>
-                  <p>Profile: ${profile}</p>
-                  <hr>
-                `;
+        const data = await response.json();
 
-                userListElement.appendChild(userDiv);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+        const userListElement = document.getElementById('userList');
+        userListElement.innerHTML = ''; // Clear existing user list
+
+        data.data.forEach(user => {
+            const name = user.attributes.name;
+            const email = user.attributes.email;
+            const profile = user.attributes.profile;
+
+            const userDiv = document.createElement('div');
+            userDiv.innerHTML = `
+                <h3>Name: ${name}</h3>
+                <p>Email: ${email}</p>
+                <p>Profile: ${profile}</p>
+                <hr>
+            `;
+
+            userListElement.appendChild(userDiv);
         });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }

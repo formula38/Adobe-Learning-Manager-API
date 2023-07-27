3da609ead2423347c1b3e8faccc44101c50df97d
formula38 <louismconley@gmail.com>
2023-07-26 17:13:50 -0700

    HTML, CSS, JS updates

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ async function getUsers() {
     try {
         const response = await fetch('https://learningmanager.adobe.com/primeapi/v2/users/mrtakata%40ucdavis.edu/accounts?onlyActive=true&socialEnabledAccounts=false');
 
-        const responseData = await response.text();
-        console.log('API Response:', responseData);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
 
         const data = await response.json();
 
         const userListElement = document.getElementById('userList');
         userListElement.innerHTML = ''; // Clear existing user list
 
-        data.data.forEach(user => {
+        data.forEach(user => {
             const name = user.attributes.subdomain;
 
             const userDiv = document.createElement('user');

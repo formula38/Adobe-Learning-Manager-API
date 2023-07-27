021156c3ea364af1766e8d454c70a012b27ddf27
formula38 <louismconley@gmail.com>
2023-07-27 14:09:45 -0700

    HTML, CSS, JS updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ async function getUsers() {
     try {
         const emailInput = document.getElementById('emailInput');
         const email = emailInput.value;
-        const encodedEmail = encodeURIComponent(email); // Encode the email address
+        const encodedEmail = encodeURIComponent(email);
 
         const apiUrl = `https://learningmanager.adobe.com/primeapi/v2/users/${encodedEmail}/accounts?onlyActive=true&socialEnabledAccounts=false`;
 
@@ -12,20 +12,32 @@ async function getUsers() {
         const userListElement = document.getElementById('userList');
         userListElement.innerHTML = ''; // Clear existing user list
 
-        data.data.forEach(user => {
-            const acctId = user.id;
-            const badge = user.attributes.logoUrl;
-
+        if (data.data.length === 0) {
+            // If the API response does not contain any data (user not found), show "No User Found" message and wrong turn sign image
             const userDiv = document.createElement('div');
             userDiv.innerHTML = `
-                <h4>Account ID: ${acctId}</h4>
-                <img src="${badge}">
-                <hr>
+                <h2>No User Found!</h2>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdjqr8xggw3YpL6bXFgBObvp46dYhBwfrLGA&usqp=CAU" class="wrong-turn-sign-image">
             `;
-
             userListElement.appendChild(userDiv);
-        });
+        } else {
+            // If the API response contains user data, display the user information as before
+            data.data.forEach(user => {
+                const acctId = user.id;
+                const badge = user.attributes.logoUrl;
+
+                const userDiv = document.createElement('div');
+                userDiv.innerHTML = `
+                    <h4>Account ID: ${acctId}</h4>
+                    <img src="${badge}" class="responsive-image">
+                    <hr>
+                `;
+
+                userListElement.appendChild(userDiv);
+            });
+        }
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
+
